bed83aafad42094a145b937b00f2ebf361704faa
wuzhzn <dagreatbao@gmail.com>
2023-07-04 19:19:30 +0800

    Fetch JSON data from NUSMOD api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.nusmods.com/v2/2023-2024/moduleInfo.json"
+        );
+        const modules = await response.json();
+        const firstModuleCode = modules[0].moduleCode;
+        setData(firstModuleCode);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -25,11 +44,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div>{data}</div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
