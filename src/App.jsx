17fb2c17776e1a8d0aa820c9652e01ca5bc22797
Derrick Khoo <derrickkhoo2000@gmail.com>
2023-07-10 22:38:10 +0800

    Resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -120,43 +120,6 @@ function App() {
         </a>
       </Navbar.Section>
     </Navbar>
-=======
-import React, { useEffect, useState } from "react";
-import { db } from "./firebase";
-import { doc, setDoc, Timestamp } from "firebase/firestore";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://api.nusmods.com/v2/2023-2024/moduleInfo.json"
-        );
-        const modules = await response.json();
-    
-        modules.forEach(async (module) => {
-          const { moduleCode, moduleCredit } = module;
-          try {
-            await setDoc(doc(db, "modules", moduleCode), { moduleCredit });
-            console.log(`Module ${moduleCode} stored in Firestore.`);
-          } catch (error) {
-            console.error(`Error storing module ${moduleCode} in Firestore:`, error);
-          }
-        });
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <h1>Main Page</h1>;
-    </>
->>>>>>> 3bd311858b6beaf1c8642ac02b32788f3585200f
   );
 }
   // return (
