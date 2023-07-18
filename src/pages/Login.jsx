590873402046d37b012944602e8447ad3b836c33
zylee348 <e0725436@u.nus.edu>
2023-07-18 18:58:02 +0800

    Add login page and remove stock code

---
+++ src/pages/Login.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [studentNumber, setStudentNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Validate the student number and password here
+    if (studentNumber === "123456" && password === "password") {
+      // Redirect to the main page after successful login
+      history.push("/main");
+    } else {
+      // Handle login failure (e.g., show error message)
+      console.log("Invalid credentials");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Student Number:
+          <input
+            type="text"
+            value={studentNumber}
+            onChange={(event) => setStudentNumber(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
