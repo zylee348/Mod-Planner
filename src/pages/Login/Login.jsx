518569c5a816e3f401adc8d2659def004d8410a7
zylee348 <e0725436@u.nus.edu>
2023-07-19 12:15:18 +0800

    Change Login page to try to send data from login to mongo

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,48 +1,112 @@
+// import { useState } from "react";
+
+// const Login = () => {
+//   const [studentNumber, setStudentNumber] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     // Validate the student number and password here
+//     if (studentNumber === "123456" && password === "password") {
+//       // Redirect to the main page after successful login
+//       history.push("/home");
+//     } else {
+//       // Handle login failure (e.g., show error message)
+//       console.log("Invalid credentials");
+//     }
+//   };
+
+//   return (
+//     <div className="login-container">
+//       <h1>Login</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Student Number:
+//           <input
+//             type="text"
+//             value={studentNumber}
+//             onChange={(event) => setStudentNumber(event.target.value)}
+//           />
+//         </label>
+//         <br />
+//         <label>
+//           Password: 
+//           <input
+//             type="password"
+//             value={password}
+//             onChange={(event) => setPassword(event.target.value)}
+//           />
+//         </label>
+//         <br />
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   const [studentNumber, setStudentNumber] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Validate the student number and password here
-    if (studentNumber === "123456" && password === "password") {
-      // Redirect to the main page after successful login
-      history.push("/home");
-    } else {
-      // Handle login failure (e.g., show error message)
-      console.log("Invalid credentials");
+    try {
+      const response = await axios.post("/register", {
+        studentNumber,
+        password,
+      });
+
+      // Assuming the server responds with a success message
+      if (response.data.success) {
+        setIsLoggedIn(true);
+      } else {
+        // Handle login failure (e.g., show error message)
+        console.log("Invalid credentials");
+      }
+    } catch (error) {
+      console.log("Error occurred during login:", error);
     }
   };
 
-  return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Student Number:
-          <input
-            type="text"
-            value={studentNumber}
-            onChange={(event) => setStudentNumber(event.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password: 
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  // Render the login form if not logged in, or redirect to home if logged in
+  if (isLoggedIn) {
+    return <Redirect to="/home" />;
+  } else {
+    return (
+      <div className="login-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Student Number:
+            <input
+              type="text"
+              value={studentNumber}
+              onChange={(event) => setStudentNumber(event.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Login;
