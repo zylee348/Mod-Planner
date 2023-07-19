59b6a715f9a55b2dabbd1ac42a642597bae94262
zylee348 <e0725436@u.nus.edu>
2023-07-19 21:29:13 +0800

    stuck at this stage of login and tryin to get to mongodb

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,81 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router"
+import axios from "axios";
+
+const Login = () => {
+  const [studentNumber, setStudentNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+      if (isLoggedIn) {
+        navigate('/home')
+      }
+    })
+    
+    try {
+      const response = axios.post("/api/auth/login", {
+        studentNumber,
+        password,
+      });
+
+      // Assuming the server responds with a success message
+      if (response.data.success) {
+        setIsLoggedIn(true);
+        setAuthState({ username: response.data.username, id: response.data.id, status: true});
+        navigate("/home");
+      } else {
+        // Handle login failure (e.g., show error message)
+        console.log("Invalid credentials");
+      }
+    } catch (error) {
+      console.log("Error occurred during login:", error);
+    }
+  };
+
+  // Render the login form if not logged in, or redirect to home if logged in
+  if (isLoggedIn) {
+    return <Redirect to="/home" />;
+  } else {
+    return (
+      <div className="login-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Student Number:
+            <input
+              type="text"
+              value={studentNumber}
+              onChange={(event) => setStudentNumber(event.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit" className="btn btn-primary">Login</button>
+        </form>
+        <p className="regs">
+            <button type="submit">Don't have an account? Sign Up</button>
+          </p>
+      </div>
+    );
+  }
+};
+
+export default Login;
+
 // import { useState } from "react";
 
 // const Login = () => {
@@ -46,67 +124,3 @@
 // };
 
 // export default Login;
-
-import { useState } from "react";
-import axios from "axios";
-
-const Login = () => {
-  const [studentNumber, setStudentNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await axios.post("/register", {
-        studentNumber,
-        password,
-      });
-
-      // Assuming the server responds with a success message
-      if (response.data.success) {
-        setIsLoggedIn(true);
-      } else {
-        // Handle login failure (e.g., show error message)
-        console.log("Invalid credentials");
-      }
-    } catch (error) {
-      console.log("Error occurred during login:", error);
-    }
-  };
-
-  // Render the login form if not logged in, or redirect to home if logged in
-  if (isLoggedIn) {
-    return <Redirect to="/home" />;
-  } else {
-    return (
-      <div className="login-container">
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Student Number:
-            <input
-              type="text"
-              value={studentNumber}
-              onChange={(event) => setStudentNumber(event.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </label>
-          <br />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    );
-  }
-};
-
-export default Login;
