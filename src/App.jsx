d6e6a2e7949b5035e47788aa5458fe151ed09915
Derrick Khoo <derrickkhoo2000@gmail.com>
2023-08-03 21:56:38 +0800

    Change from firestore to realtime database

--- src/App.jsx
+++ src/App.jsx
@@ -120,30 +120,30 @@ function App() {
     </a>
   ));
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://api.nusmods.com/v2/2023-2024/moduleInfo.json"
-        );
-        const modules = await response.json();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         "https://api.nusmods.com/v2/2023-2024/moduleInfo.json"
+  //       );
+  //       const modules = await response.json();
     
-        modules.forEach(async (module) => {
-          const { moduleCode, moduleCredit } = module;
+  //       modules.forEach(async (module) => {
+  //         const { moduleCode, moduleCredit } = module;
     
-          try {
-            await setDoc(doc(db, "modules", moduleCode), { moduleCredit });
-            console.log("Module ${moduleCode} stored in Firestore.");
-          } catch (error) {
-            console.error("Error storing module ${moduleCode} in Firestore, error");
-          }
-        });
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
+  //         try {
+  //           await setDoc(doc(db, "modules", moduleCode), { moduleCredit });
+  //           console.log("Module ${moduleCode} stored in Firestore.");
+  //         } catch (error) {
+  //           console.error("Error storing module ${moduleCode} in Firestore, error");
+  //         }
+  //       });
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <AppShell
