071cde2ab24c69700cf0f9f411186857005fb4ce
zylee348 <e0725436@u.nus.edu>
2023-08-04 22:55:05 +0800

    Change to logout button position

--- src/App.jsx
+++ src/App.jsx
@@ -156,26 +156,6 @@ function App() {
             </Group>
             {links}
           </Navbar.Section>
-
-          <Navbar.Section className={classes.footer}>
-            <a
-              href="#"
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
-              <span>Change account</span>
-            </a>
-
-            <a
-              href="#"
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              <IconLogout className={classes.linkIcon} stroke={1.5} />
-              <span>Logout</span>
-            </a>
-          </Navbar.Section>
         </Navbar>
       }
       layout="alt"
